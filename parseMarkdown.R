ec2df9e702f426ad79ed5b8c845dcb0ea836b1a7
Added header for item results <JBryer@excelsior.edu>
2017-04-10 16:53:53 -0400

    Added message for debugging.

--- parseMarkdown.R
+++ parseMarkdown.R
@@ -2,8 +2,10 @@ parseMarkdown <- function(dir) {
 	results <- list()
 	mdfiles <- list.files(path = dir, pattern = '*.md', recursive = FALSE)
 	for(i in mdfiles) {
+		mdfile <- path.expand(paste0(dir, '/', i))
+		message(paste0('Parsing ', mdfile, '...'))
 		tmp <- markdownToHTML(
-			path.expand(paste0(dir, '/', i)), fragment.only = TRUE)
+			mdfile, fragment.only = TRUE)
 		results[[file_path_sans_ext(basename(i))]] <- gsub('\\\\', '', tmp)
 	}
 	dirs <- list.dirs(path = dir, recursive = FALSE)
